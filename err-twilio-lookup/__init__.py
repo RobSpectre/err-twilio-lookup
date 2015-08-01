6e0f0340d14a3cffbaec13eea066709764f5251e
Rob Spectre <rob@brooklynhacker.com>
2015-08-01 15:50:44 -0400

    1) Feature complete. 2) Added test coverage.

--- err-twilio-lookup/__init__.py
+++ err-twilio-lookup/__init__.py
@@ -4,6 +4,7 @@ from errbot import BotPlugin
 from errbot import botcmd
 
 from twilio.rest.lookups import TwilioLookupsClient
+from twilio.rest.exceptions import TwilioRestException
 
 import phonenumbers
 
@@ -12,17 +13,16 @@ class TwilioLookup(BotPlugin):
     """An err plugin for retrieving information on phone numbers using Twilio
     Lookup."""
     min_err_version = '1.6.0'
-    max_err_version = '2.3.0'
+    max_err_version = '3.0.0'
 
     def activate(self):
-        super(TwilioLookup, self).activate()
-
         if self.config is not None:
             self.TWILIO_ACCOUNT_SID = self.config.get('TWILIO_ACCOUNT_SID',
                                                       None)
             self.TWILIO_AUTH_TOKEN = self.config.get('TWILIO_AUTH_TOKEN',
                                                      None)
             if self.TWILIO_ACCOUNT_SID and self.TWILIO_AUTH_TOKEN:
+                super(TwilioLookup, self).activate()
                 self.lookup = TwilioLookupsClient(self.TWILIO_ACCOUNT_SID,
                                                   self.TWILIO_AUTH_TOKEN)
             else:
@@ -33,4 +33,62 @@ class TwilioLookup(BotPlugin):
 
     @botcmd
     def lookup(self, message, args):
-        return message
+        number = self.sanitize_number(args)
+
+        if hasattr(number, "country_code"):
+            formatter = phonenumbers.PhoneNumberFormat.E164
+            number = phonenumbers.format_number(number, formatter)
+            number = self.lookup_e164_number(number)
+
+            if hasattr(number, "national_format"):
+                yield "I found a {0} {1} number " \
+                      "{2}.".format(number.country_code,
+                                    number.carrier['type'],
+                                    number.national_format)
+
+                if number.carrier['name'] == "Twilio":
+                    yield "My heart glows as it is a :twilio: Twilio " \
+                          "number :twilio:."
+                else:
+                    yield "It tragically belongs to " \
+                          "{0}".format(number.carrier['name'])
+
+        yield number
+
+    def lookup_e164_number(self, number_string):
+        try:
+            return self.lookup.phone_numbers.get(number_string,
+                                                 include_carrier_info=True)
+        except TwilioRestException as e:
+            return "Could not find information on phone number " \
+                   "{0}: {1}".format(number_string, e)
+
+    def sanitize_number(self, number):
+        if not any(character.isdigit() for character in number):
+            return "Could not find a number here: " \
+                   "{0}".format(number)
+        if any(character.isalpha() for character in number):
+            split = number.split(" ")
+            if len(split) == 2:
+                number = phonenumbers.parse(split[0], split[1])
+            else:
+                # Gross hack as PhoneNumberMatcher doesn't support indexing.
+                for match in phonenumbers.PhoneNumberMatcher(number, "US"):
+                    number = match.number
+                    break
+        else:
+            number = phonenumbers.parse(number)
+
+        number = self.validate_number(number)
+
+        return number
+
+    def validate_number(self, number):
+        if not phonenumbers.is_possible_number(number):
+            return "This number is not possible: " \
+                   "{0}".format(number)
+        if not phonenumbers.is_valid_number(number):
+            return "This number is not valid: " \
+                   "{0}".format(number)
+
+        return number
--- tests/test_err-twilio-lookup.py
+++ tests/test_err-twilio-lookup.py
@@ -1,17 +1,193 @@
-from errbot.backends.test import testbot
-from errbot import plugin_manager
+import logging
+
+from unittest.mock import patch
+from unittest.mock import Mock
+
+from errbot.backends.test import FullStackTest
+
+from twilio.rest.exceptions import TwilioRestException
+
+config = {
+    'TWILIO_ACCOUNT_SID': 'ACxxxxx',
+    'TWILIO_AUTH_TOKEN': 'yyyyyyyy'
+}
 
 
-class TestTwilioLookup(object):
-    extra_plugin_dir = '.'
+def create_mock_json(path):
+    with open(path) as f:
+        resp = Mock()
+        resp.content = f.read()
+        return resp
 
-    def setUp(self):
-        self.plugin = plugin_manager.get_plugin_obj_by_name('TwilioLookup')
 
-    def test_lookup(self, testbot):
-        expected = "test"
-        self.bot.push_message("!lookup")
+class TestTwilioLookup(FullStackTest):
+    def setUp(self, extra_plugin_dir='.', logging_level=logging.INFO):
+        super(TestTwilioLookup, self).setUp(extra_plugin_dir=extra_plugin_dir,
+                                            loglevel=logging_level)
 
-        self.assertTrue(expected in self.bot.pop_message(),
-                        "Did not get message 'test', instead: "
-                        "{0}".format(self.bot.pop_message()))
+        self.bot.set_plugin_configuration('TwilioLookup', config)
+        self.bot.activate_plugin('TwilioLookup')
+
+        self.plugin = self.bot.get_plugin_obj_by_name('TwilioLookup')
+
+    def assertCommand(self, command, response, timeout=5):
+        self.bot.push_message(command)
+        self.assertIn(response, self.bot.pop_message(timeout),
+                      "Did not find expected response to command "
+                      "{0}: {1}".format(command, response))
+
+
+class TestTwilioLookupCommands(TestTwilioLookup):
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_lookup(self, request):
+        response = create_mock_json(
+            "tests/resources/phone_number_instance.json"
+        )
+
+        request.return_value = response
+
+        self.assertCommand('!lookup +15108675309',
+                           'I found a US mobile number (510) 867-5309.')
+        request.assert_called_with('GET',
+                                   'https://lookups.twilio.com/v1'
+                                   '/PhoneNumbers/+15108675309',
+                                   auth=('ACxxxxx', 'yyyyyyyy'),
+                                   params={'Type': 'carrier'},
+                                   use_json_extension=False)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_lookup_with_country_code(self, request):
+        response = create_mock_json(
+            "tests/resources/phone_number_instance.json"
+        )
+
+        request.return_value = response
+
+        self.assertCommand('!lookup (510)867-5309 US',
+                           'I found a US mobile number (510) 867-5309.')
+        request.assert_called_with('GET',
+                                   'https://lookups.twilio.com/v1'
+                                   '/PhoneNumbers/+15108675309',
+                                   auth=('ACxxxxx', 'yyyyyyyy'),
+                                   params={'Type': 'carrier'},
+                                   use_json_extension=False)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_lookup_with_twilio_number(self, request):
+        response = create_mock_json(
+            "tests/resources/phone_number_instance_twilio.json"
+        )
+
+        request.return_value = response
+
+        self.bot.push_message("!lookup +15108675309")
+        self.bot.pop_message()
+        self.assertIn("Twilio", self.bot.pop_message())
+        request.assert_called_with('GET',
+                                   'https://lookups.twilio.com/v1'
+                                   '/PhoneNumbers/+15108675309',
+                                   auth=('ACxxxxx', 'yyyyyyyy'),
+                                   params={'Type': 'carrier'},
+                                   use_json_extension=False)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_lookup_with_crap_input(self, request):
+        response = create_mock_json(
+            "tests/resources/phone_number_instance_twilio.json"
+        )
+
+        request.return_value = response
+
+        self.bot.push_message("!lookup Hey there - can you look up"
+                              " (510) 8675309?")
+        self.bot.pop_message()
+        self.assertIn("Twilio", self.bot.pop_message())
+        request.assert_called_with('GET',
+                                   'https://lookups.twilio.com/v1'
+                                   '/PhoneNumbers/+15108675309',
+                                   auth=('ACxxxxx', 'yyyyyyyy'),
+                                   params={'Type': 'carrier'},
+                                   use_json_extension=False)
+
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_lookup_handle_rest_error(self, request):
+        def raiseException(*args, **kwargs):
+            raise TwilioRestException("Test error.", "/error")
+
+        request.side_effect = raiseException
+
+        self.bot.push_message("!lookup +15108675309")
+
+        self.assertIn("Could not find information on phone number",
+                      self.bot.pop_message())
+
+
+class TestTwilioLookupUtilities(TestTwilioLookup):
+    @patch("twilio.rest.resources.base.make_twilio_request")
+    def test_lookup_164_number(self, request):
+        response = create_mock_json(
+            "tests/resources/phone_number_instance.json"
+        )
+
+        expected = "(510) 867-5309"
+
+        request.return_value = response
+
+        result = self.plugin.lookup_e164_number("+15108675309")
+        request.assert_called_with('GET',
+                                   'https://lookups.twilio.com/v1'
+                                   '/PhoneNumbers/+15108675309',
+                                   auth=('ACxxxxx', 'yyyyyyyy'),
+                                   params={'Type': 'carrier'},
+                                   use_json_extension=False)
+        self.assertEquals(expected, result.national_format)
+
+    def test_sanitize_number_e164(self):
+        expected = "+15108675309"
+        result = self.plugin.sanitize_number(expected)
+
+        self.assertEquals(1, result.country_code)
+        self.assertEquals(5108675309, result.national_number)
+
+    def test_sanitize_number_with_country_code(self):
+        expected = "(510)867-5309 US"
+        result = self.plugin.sanitize_number(expected)
+        self.assertEquals(1, result.country_code)
+        self.assertEquals(5108675309, result.national_number)
+
+    def test_sanitize_number_not_valid(self):
+        expected = "This number is not valid: Country Code: 1 National " \
+                   "Number: 5556667777"
+        result = self.plugin.sanitize_number("+15556667777")
+        self.assertEquals(expected, result)
+
+    def test_sanitize_number_not_possible(self):
+        expected = "This number is not possible: Country Code: 1 National " \
+                   "Number: 555666777"
+        result = self.plugin.sanitize_number("+1555666777")
+        self.assertEquals(expected, result)
+
+    def test_sanitize_number_no_digits(self):
+        expected = "Could not find a number in this input: error"
+        result = self.plugin.sanitize_number("error")
+        self.assertEquals(expected, result)
+
+
+class TestTwilioLookupWithoutConfiguration(FullStackTest):
+    def setUp(self, extra_plugin_dir='.', logging_level=logging.INFO):
+        super(TestTwilioLookupWithoutConfiguration,
+              self).setUp(extra_plugin_dir=extra_plugin_dir,
+                          loglevel=logging_level)
+
+    def test_no_configuration(self):
+        plugin_list = self.bot.get_all_active_plugin_names()
+        self.assertFalse("TwilioLookup" in plugin_list)
+
+    def test_wrong_configuration(self):
+        incomplete_configuration = {"TWILIO_ACCOUNT_SID": "ACxxxx"}
+        self.bot.set_plugin_configuration('TwilioLookup',
+                                          incomplete_configuration)
+        self.bot.activate_plugin('TwilioLookup')
+
+        plugin_list = self.bot.get_all_active_plugin_names()
+        self.assertFalse("TwilioLookup" in plugin_list)
